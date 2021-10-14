2d4e7729534096ac7bfe1395aae3507f4b72b689
Teddy Byron <ted@tedbyron.com>
2021-10-14 17:22:57 -0400

    add pokeapi wrapper crate

---
+++ pokeapi-rs/README.md
@@ -0,0 +1,7 @@
+<div align="center">
+  <h1><code>pokeapi-rs</code></h1>
+
+  <p>
+    <strong>Rust PokéAPI bindings.</strong>
+  </p>
+</div>
---
+++ pokeapi-rs/src/lib.rs
@@ -0,0 +1,9 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
